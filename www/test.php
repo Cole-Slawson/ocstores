7ed6a26f8b20710e466a8195f91e7b669eb2d44a
Sinergize <oluwatobiotemuyiwa@gmail.com>
2017-03-28 18:08:07 +0100

    done creating uploads section

--- www/test.php
+++ www/test.php
@@ -14,9 +14,51 @@
     } catch(PDOException $e) {
     	echo $e->getMessage();
     }*/
+#max file size...
+define("MAX_FILE_SIZE", "2097152");
+
+#allowed extension..
+$ext = ["image/jpg", "image/jpeg", "image/png"];
 
 if(array_key_exists('save', $_POST)) {
-	print_r($_FILES);
+
+     $error= array();
+	#be sure a file was selected..
+	  if(empty($_FILES['pic']['name'])) {
+	  	  $errors[] = "please choose a file";
+	  }
+	  #check file size...
+	  if ($_FILES['pic']['size'] > MAX_FILE_SIZE) {
+	  	 $errors[] = "file size exceeds maximum. maximum: ".MAX_FILE_SIZE;
+	  	
+	  }
+
+	  # check file size...
+	  if (!in_array($_FILES['pic']['type'], $ext)) {
+	  	       $errors[] = "invalid file type";
+	  }
+	  #generate random number to append
+	  $rnd = rand(0000, 9999);
+
+	  #strip filename for spaces
+	  $strip_name = str_replace("", "_", $_FILES['pic']['name']);
+
+	  $filename = $rnd.$strip_name;
+	  $destination = 'uploads/'.$filename;
+
+	  if (!move_uploaded_file($_FILES['pic']['tmp_name'], $destination)) {
+	  	     $errors[] = "file uploaded failed";
+	  }
+
+	  if (empty($errors)) {
+	  	echo "done";
+	  	
+	  }else{
+	  	foreach ($errors as $err) {
+	  		echo $err. '</br>';
+	  		
+	  	}
+	  }
 }
 
 ?>
